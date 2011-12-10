f8a80432264bce970084f8e34249fa55c4538cb9
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2011-12-10 14:05:10 -0600

    Proyecto Rails inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tururu::Application.config.secret_token = '2b214cd735a01e84704edad28f070ecf36eb39d07086d5818be6180a42ce9b6739624dc26f7e5454d3830581a07f799f188aaf6371754a526a5634536c34b349'
